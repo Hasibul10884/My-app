d38b9f0839ff0abfd4047b2aea42856b023e6441
Hasibul10884 <hasibulhasan193@gmail.com>
2022-03-29 20:23:53 +0600

    Initial commit

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView btn_1, btn_2, btn_3, btn_4, btn_5, btn_6, btn_7, btn_8, btn_9, btn_0;
+    ImageView btn_dot, btn_equal, btn_ac, btn_module, btn_plus, btn_minus, btn_multiplication;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
